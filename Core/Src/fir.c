93fd34863b017947c890a2e353b8e4bcfa42ff6c
Ilya Pikin <hypnotiod@gmail.com>
2021-04-15 22:40:30 +0300

    Removed input signal amplification

--- Core/Src/fir.c
+++ Core/Src/fir.c
@@ -2,8 +2,6 @@
 #include "fir.h"
 #include "spi.h"
 
-#define INPUT_AMPLIFY_FACTOR 12
-
 volatile bool FIR_dataReady = false;
 
 static int32_t _buffer[AUDIO_CIRCULAR_BUFFER_QUARTER_SIZE] = {0};
@@ -19,33 +17,16 @@ void FIR_Init(void)
 void FIR_Transfer(int16_t * pBuff)
 {
   size_t i;
-  int16_t sample;
   
   for (i = 0; i < AUDIO_CIRCULAR_BUFFER_HALF_SIZE; i += FIR_PACKET_SAMPLES_NUM * sizeof(uint16_t)) {
-    sample = *pBuff * INPUT_AMPLIFY_FACTOR;
-    ((uint16_t *) _buffer)[i]   = AUDIO_Swap16(sample);
-    pBuff += 2;
-    sample = *pBuff * INPUT_AMPLIFY_FACTOR;
-    ((uint16_t *) _buffer)[i+1] = AUDIO_Swap16(sample);
-    pBuff += 2;
-    sample = *pBuff * INPUT_AMPLIFY_FACTOR;
-    ((uint16_t *) _buffer)[i+2] = AUDIO_Swap16(sample);
-    pBuff += 2;
-    sample = *pBuff * INPUT_AMPLIFY_FACTOR;
-    ((uint16_t *) _buffer)[i+3] = AUDIO_Swap16(sample);
-    pBuff += 2;
-    sample = *pBuff * INPUT_AMPLIFY_FACTOR;
-    ((uint16_t *) _buffer)[i+4] = AUDIO_Swap16(sample);
-    pBuff += 2;
-    sample = *pBuff * INPUT_AMPLIFY_FACTOR;
-    ((uint16_t *) _buffer)[i+5] = AUDIO_Swap16(sample);
-    pBuff += 2;
-    sample = *pBuff * INPUT_AMPLIFY_FACTOR;
-    ((uint16_t *) _buffer)[i+6] = AUDIO_Swap16(sample);
-    pBuff += 2;
-    sample = *pBuff * INPUT_AMPLIFY_FACTOR;
-    ((uint16_t *) _buffer)[i+7] = AUDIO_Swap16(sample);
-    pBuff += 2;
+    ((uint16_t *) _buffer)[i]   = AUDIO_Swap16(pBuff[0]);
+    ((uint16_t *) _buffer)[i+1] = AUDIO_Swap16(pBuff[2]);
+    ((uint16_t *) _buffer)[i+2] = AUDIO_Swap16(pBuff[4]);
+    ((uint16_t *) _buffer)[i+3] = AUDIO_Swap16(pBuff[6]);
+    ((uint16_t *) _buffer)[i+4] = AUDIO_Swap16(pBuff[8]);
+    ((uint16_t *) _buffer)[i+5] = AUDIO_Swap16(pBuff[10]);
+    ((uint16_t *) _buffer)[i+6] = AUDIO_Swap16(pBuff[12]);
+    ((uint16_t *) _buffer)[i+7] = AUDIO_Swap16(pBuff[14]);
     ((int16_t *) _buffer)[i+8] = 0;
     ((int16_t *) _buffer)[i+9] = 0;
     ((int16_t *) _buffer)[i+10] = 0;
@@ -54,6 +35,7 @@ void FIR_Transfer(int16_t * pBuff)
     ((int16_t *) _buffer)[i+13] = 0;
     ((int16_t *) _buffer)[i+14] = 0;
     ((int16_t *) _buffer)[i+15] = 0;
+    pBuff += 16;
   }
   
   FIR_CSLow();
